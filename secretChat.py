fc52473dddc2ead8ea6b83be08233756bc572db2
Kiefer <kmsivitz@gmail.com>
2016-12-14 17:42:23 -0500

    Fixed 'Exit' Menu Action

--- secretChat.py
+++ secretChat.py
@@ -2,31 +2,31 @@
 
 # Form implementation generated from reading ui file 'secretChat.ui'
 #
-# Created by: PyQt4 UI code generator 4.11.4
+# Created by: PyQt5 UI code generator 5.5.1
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 import sys, socket
-from thread import *
+from _thread import *
     
 def app_version():
     msg_box("Application Version", "Null-Byte P2P Chat v1.0")
 
 def msg_box(title, data):
-    w = QtGui.QWidget()
-    QtGui.QMessageBox.information(w, title, data)
+    w = QtWidgets.QWidget()
+    QtWidgets.QMessageBox.information(w, title, data)
 
 def update_list(self, data):
     self.listWidget.addItem(data)
-    print"\a"
+    print("\a")
 
 def server_socket(self):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(('', 6190))
         s.listen(1)
-    except socket.error, e:
+    except (socket.error, e):
         msg_box("Socket Error!", "Unable to Setup Local Socket, Port In Use")
         return
 
@@ -64,102 +64,104 @@ class Ui_MainWindow(object):
 
         self.start_server()
 
-        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.setObjectName("MainWindow")
         MainWindow.resize(655, 346)
-        self.centralwidget = QtGui.QWidget(MainWindow)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.frame = QtGui.QFrame(self.centralwidget)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
         self.frame.setGeometry(QtCore.QRect(0, 0, 651, 31))
-        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
-        self.frame.setFrameShadow(QtGui.QFrame.Raised)
-        self.frame.setObjectName(_fromUtf8("frame"))
-        self.label = QtGui.QLabel(self.frame)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.label = QtWidgets.QLabel(self.frame)
         self.label.setGeometry(QtCore.QRect(10, 10, 71, 17))
-        self.label.setObjectName(_fromUtf8("label"))
-        self.label_2 = QtGui.QLabel(self.frame)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.frame)
         self.label_2.setGeometry(QtCore.QRect(340, 10, 61, 20))
-        self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.lineEdit = QtGui.QLineEdit(self.frame)
+        self.label_2.setObjectName("label_2")
+        self.lineEdit = QtWidgets.QLineEdit(self.frame)
         self.lineEdit.setGeometry(QtCore.QRect(90, 0, 241, 27))
-        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
-        self.lineEdit_2 = QtGui.QLineEdit(self.frame)
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.frame)
         self.lineEdit_2.setGeometry(QtCore.QRect(410, 0, 241, 27))
-        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
-        self.frame_2 = QtGui.QFrame(self.centralwidget)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.frame_2 = QtWidgets.QFrame(self.centralwidget)
         self.frame_2.setGeometry(QtCore.QRect(0, 30, 331, 251))
-        self.frame_2.setFrameShape(QtGui.QFrame.StyledPanel)
-        self.frame_2.setFrameShadow(QtGui.QFrame.Raised)
-        self.frame_2.setObjectName(_fromUtf8("frame_2"))
-        self.textEdit = QtGui.QTextEdit(self.frame_2)
+        self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_2.setObjectName("frame_2")
+        self.textEdit = QtWidgets.QTextEdit(self.frame_2)
         self.textEdit.setGeometry(QtCore.QRect(0, 10, 331, 201))
-        self.textEdit.setObjectName(_fromUtf8("textEdit"))
-        self.pushButton_3 = QtGui.QPushButton(self.frame_2)
+        self.textEdit.setObjectName("textEdit")
+        self.pushButton_3 = QtWidgets.QPushButton(self.frame_2)
         self.pushButton_3.setGeometry(QtCore.QRect(0, 210, 211, 41))
-        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        self.pushButton_3.setObjectName("pushButton_3")
 
         #############################################################
         # Executes when send message button is clicked
         self.pushButton_3.clicked.connect(self.client_send_message)
         #############################################################
 
-        self.pushButton_4 = QtGui.QPushButton(self.frame_2)
+        self.pushButton_4 = QtWidgets.QPushButton(self.frame_2)
         self.pushButton_4.setGeometry(QtCore.QRect(210, 210, 121, 41))
-        self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
+        self.pushButton_4.setObjectName("pushButton_4")
 
         #############################################################
         # Executes on clear log button click
         self.pushButton_4.clicked.connect(self.client_send_message)
         #############################################################
 
-        self.frame_3 = QtGui.QFrame(self.centralwidget)
+        self.frame_3 = QtWidgets.QFrame(self.centralwidget)
         self.frame_3.setGeometry(QtCore.QRect(330, 30, 321, 251))
-        self.frame_3.setFrameShape(QtGui.QFrame.StyledPanel)
-        self.frame_3.setFrameShadow(QtGui.QFrame.Raised)
-        self.frame_3.setObjectName(_fromUtf8("frame_3"))
-        self.listWidget = QtGui.QListWidget(self.frame_3)
+        self.frame_3.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_3.setObjectName("frame_3")
+        self.listWidget = QtWidgets.QListWidget(self.frame_3)
         self.listWidget.setGeometry(QtCore.QRect(10, 10, 301, 241))
-        self.listWidget.setObjectName(_fromUtf8("listWidget"))
+        self.listWidget.setObjectName("listWidget")
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 655, 27))
-        self.menubar.setObjectName(_fromUtf8("menubar"))
-        self.menuMenu_Actions = QtGui.QMenu(self.menubar)
-        self.menuMenu_Actions.setObjectName(_fromUtf8("menuMenu_Actions"))
+        self.menubar.setObjectName("menubar")
+        self.menuMenu_Actions = QtWidgets.QMenu(self.menubar)
+        self.menuMenu_Actions.setObjectName("menuMenu_Actions")
         MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtGui.QStatusBar(MainWindow)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-        self.actionVersion = QtGui.QAction(MainWindow)
-        self.actionVersion.setObjectName(_fromUtf8("actionVersion"))
+        self.actionVersion = QtWidgets.QAction(MainWindow)
+        self.actionVersion.setObjectName("actionVersion")
 
         ##############################################################
         # Executes when submenu item version is clicked
         self.actionVersion.triggered.connect(app_version)
         ##############################################################
 
-        self.actionExit = QtGui.QAction(MainWindow)
-        self.actionExit.setObjectName(_fromUtf8("actionExit"))
+        self.actionExit = QtWidgets.QAction(MainWindow)
+        self.actionExit.setObjectName("actionExit")
 
         ##############################################################
         # Executes when the submenu item exit is clicked
-        #self.actionExit.triggered.connect(qApp.quit)
+        self.actionExit.triggered.connect(QtCore.QCoreApplication.instance().quit)
         ##############################################################
 
         self.menuMenu_Actions.addAction(self.actionVersion)
         self.menuMenu_Actions.addAction(self.actionExit)
         self.menubar.addAction(self.menuMenu_Actions.menuAction())
+
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
-        self.label.setText(_translate("MainWindow", "IP Address:", None))
-        self.label_2.setText(_translate("MainWindow", "Nickname:", None))
-        self.pushButton_3.setText(_translate("MainWindow", "Send Message", None))
-        self.pushButton_4.setText(_translate("MainWindow", "Clear Logs", None))
-        self.menuMenu_Actions.setTitle(_translate("MainWindow", "Menu Actions", None))
-        self.actionVersion.setText(_translate("MainWindow", "Version", None))
-        self.actionExit.setText(_translate("MainWindow", "Exit", None))
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "IP Address:"))
+        self.label_2.setText(_translate("MainWindow", "Nickname:"))
+        self.pushButton_3.setText(_translate("MainWindow", "Send Message"))
+        self.pushButton_4.setText(_translate("MainWindow", "Clear Logs"))
+        self.menuMenu_Actions.setTitle(_translate("MainWindow", "Menu Actions"))
+        self.actionVersion.setText(_translate("MainWindow", "Version"))
+        self.actionExit.setText(_translate("MainWindow", "Exit"))
 
     def clear_logs(self):
         self.listWidget.clear()
@@ -182,7 +184,7 @@ class Ui_MainWindow(object):
 
         try:
             c.connect((ip_address, 6190))
-        except Exception, e:
+        except (Exception, e):
             msg_box("Connection Refused", "The address you are trying to reach is currently unavailable")
             return
 
@@ -190,15 +192,15 @@ class Ui_MainWindow(object):
             c.send(rmsg)
             self.listWidget.addItem(rmsg)
             self.textEdit.setText("")
-        except Exception, e:
+        except (Exception, e):
             msg_box("Connection Refused", "The message cannot be sent. Endpoint not connected")
 
         c.close()
 
 if __name__ == "__main__":
     import sys
-    app = QtGui.QApplication(sys.argv)
-    MainWindow = QtGui.QMainWindow()
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
