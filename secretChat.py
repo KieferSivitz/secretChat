4f7ce02223800bcac8b244b8fbfbf8740bf62a9d
Kiefer <kmsivitz@gmail.com>
2016-12-20 17:52:11 -0500

    UI Changes

--- secretChat.py
+++ secretChat.py
@@ -62,7 +62,7 @@ class Ui_MainWindow(object):
         self.start_server()
 
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(655, 346)
+        MainWindow.resize(655, 420)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.frame = QtWidgets.QFrame(self.centralwidget)
@@ -71,27 +71,39 @@ class Ui_MainWindow(object):
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
         self.label = QtWidgets.QLabel(self.frame)
-        self.label.setGeometry(QtCore.QRect(10, 10, 71, 17))
+        self.label.setGeometry(QtCore.QRect(10, 0, 71, 31))
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.frame)
-        self.label_2.setGeometry(QtCore.QRect(340, 10, 61, 20))
+        self.label_2.setGeometry(QtCore.QRect(340, -1, 71, 31))
         self.label_2.setObjectName("label_2")
         self.lineEdit = QtWidgets.QLineEdit(self.frame)
-        self.lineEdit.setGeometry(QtCore.QRect(90, 0, 241, 27))
+        self.lineEdit.setGeometry(QtCore.QRect(90, 0, 241, 31))
         self.lineEdit.setObjectName("lineEdit")
         self.lineEdit_2 = QtWidgets.QLineEdit(self.frame)
-        self.lineEdit_2.setGeometry(QtCore.QRect(410, 0, 241, 27))
+        self.lineEdit_2.setGeometry(QtCore.QRect(420, 0, 231, 31))
         self.lineEdit_2.setObjectName("lineEdit_2")
         self.frame_2 = QtWidgets.QFrame(self.centralwidget)
-        self.frame_2.setGeometry(QtCore.QRect(0, 30, 331, 251))
+        self.frame_2.setGeometry(QtCore.QRect(0, 30, 651, 251))
         self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame_2.setObjectName("frame_2")
-        self.textEdit = QtWidgets.QTextEdit(self.frame_2)
-        self.textEdit.setGeometry(QtCore.QRect(0, 10, 331, 201))
-        self.textEdit.setObjectName("textEdit")
-        self.pushButton_3 = QtWidgets.QPushButton(self.frame_2)
-        self.pushButton_3.setGeometry(QtCore.QRect(0, 210, 211, 41))
+        self.listWidget = QtWidgets.QListWidget(self.frame_2)
+        self.listWidget.setGeometry(QtCore.QRect(0, 10, 651, 241))
+        self.listWidget.setObjectName("listWidget")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(570, 330, 81, 31))
+        self.pushButton_4.setObjectName("pushButton_4")
+
+        #############################################################
+        # Executes on clear log button click
+        self.pushButton_4.clicked.connect(self.clear_logs)
+        #############################################################
+
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(570, 290, 81, 31))
+        self.pushButton_3.setAutoDefault(False)
+        self.pushButton_3.setDefault(False)
+        self.pushButton_3.setFlat(False)
         self.pushButton_3.setObjectName("pushButton_3")
 
         #############################################################
@@ -99,23 +111,9 @@ class Ui_MainWindow(object):
         self.pushButton_3.clicked.connect(self.client_send_message)
         #############################################################
 
-        self.pushButton_4 = QtWidgets.QPushButton(self.frame_2)
-        self.pushButton_4.setGeometry(QtCore.QRect(210, 210, 121, 41))
-        self.pushButton_4.setObjectName("pushButton_4")
-
-        #############################################################
-        # Executes on clear log button click
-        self.pushButton_4.clicked.connect(self.client_send_message)
-        #############################################################
-
-        self.frame_3 = QtWidgets.QFrame(self.centralwidget)
-        self.frame_3.setGeometry(QtCore.QRect(330, 30, 321, 251))
-        self.frame_3.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frame_3.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frame_3.setObjectName("frame_3")
-        self.listWidget = QtWidgets.QListWidget(self.frame_3)
-        self.listWidget.setGeometry(QtCore.QRect(10, 10, 301, 241))
-        self.listWidget.setObjectName("listWidget")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(0, 290, 561, 31))
+        self.textEdit.setObjectName("textEdit")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 655, 27))
@@ -154,8 +152,8 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "Secret Chat"))
         self.label.setText(_translate("MainWindow", "IP Address:"))
         self.label_2.setText(_translate("MainWindow", "Nickname:"))
-        self.pushButton_3.setText(_translate("MainWindow", "Send Message"))
         self.pushButton_4.setText(_translate("MainWindow", "Clear Logs"))
+        self.pushButton_3.setText(_translate("MainWindow", "Send"))
         self.menuMenu_Actions.setTitle(_translate("MainWindow", "Menu Actions"))
         self.actionVersion.setText(_translate("MainWindow", "Version"))
         self.actionExit.setText(_translate("MainWindow", "Exit"))
@@ -166,20 +164,24 @@ class Ui_MainWindow(object):
     def start_server(self):
         start_new_thread(server_socket, (self,))
 
+    def keyPressEvent(self, qKeyEvent):
+        print(qKeyEvent.key())
+        if qKeyEvent.key() == QtCore.Qt.Key_Return:
+            self.pushButton_3.clicked.connect(self.client_send_message)
+
     def client_send_message(self):
         ip_address = self.lineEdit.text()
 
         nick = self.lineEdit_2.text()
-        nick = nick.replace("#>", "")
+        nick = nick.replace(":", "")
         rmessage = self.textEdit.toPlainText()
-        rmessage = rmessage.replace("#>", "")
+        rmessage = rmessage.replace(":", "")
 
-        rmsg = nick + "#> " + rmessage
+        rmsg = nick + ": " + rmessage
         encodedMessage = bytes(rmsg, encoding='utf-8')
 
 
         try:
-            #c.connect((ip_address, 6190))
             c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             c.connect((ip_address, 6190))
         except Exception as e:
@@ -194,6 +196,7 @@ class Ui_MainWindow(object):
             msg_box("Connection Refused", "The message cannot be sent. Endpoint not connected")
 
         c.close()
+
 
 if __name__ == "__main__":
     import sys
